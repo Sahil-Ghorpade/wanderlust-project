b67e526b3a03d0c643c3d4cb7f231ed8f0e0102a
Sahil-Ghorpade <ghorpadesahil15@gmail.com>
2025-12-18 15:56:50 +0530

    Price after tax changes, map zoom increase

--- public/JS/map.js
+++ public/JS/map.js
@@ -5,7 +5,7 @@ mapboxgl.accessToken = mapToken;
         container: 'map',
         style: 'mapbox://styles/mapbox/standard', // Use the standard style for the map
         projection: 'globe', // display the map as a globe
-        zoom: 9, // initial zoom level, 0 is the world view, higher values zoom in
+        zoom: 15, // initial zoom level, 0 is the world view, higher values zoom in
         center: listing.geometry.coordinates, // center the map on this longitude and latitude
     });
 
